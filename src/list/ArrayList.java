3e8a92c0f32d1cc4df22c525190c06f82b556f2a
329731984@qq.com <329731984@qq.com>
2019-02-22 01:04:38 +0800

    添加list相关知识

--- src/list/ArrayList.java
+++ src/list/ArrayList.java
@@ -5,4 +5,8 @@ package list;
  */
 public class ArrayList {
     //list常用方法
+    //list 是collection 下的子包，还有set
+    // list 下有LinkedList 和ArrayList
+    //ArrayList  底层是基于数组 ，查询快，增删慢
+    //LinkedList 底层是基于双向链表，增删快，查询慢
 }
